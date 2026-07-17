93efa05ba3fdb6ca6a3fa95138733121b45f171d
jmgorecurso-lang <jmgorecurso@gmail.com>
2026-07-17 15:01:36 +0200

    Delete proyectoW directory

--- proyectoW/js/script.js
+++
@@ -1,4 +0,0 @@
-function visible(){
-    document.querySelector(".submenu").style.background="black";
-    document.querySelector(".containerUtils").style.display='block';
-}
--- proyectoW/scriptpruebas.js
+++
@@ -1,6 +0,0 @@
-const boton = document.getElementById("btnMenu");
-const menu = document.getElementById("menu");
-
-boton.addEventListener("click", () => {
-    menu.classList.toggle("mostrar");
-});
